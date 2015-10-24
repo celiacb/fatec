65d9421163f299f344be0e42be5c25cbcff24499
celiacb <celiacb@hotmail.com>
2015-10-24 11:18:58 -0200

    Teste de inserção atraves do controle

--- sceweb-v1/src/br/sceweb/teste/UC01CadastrarEmpresa.java
+++ sceweb-v1/src/br/sceweb/teste/UC01CadastrarEmpresa.java
@@ -6,17 +6,20 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.sceweb.modelo.Controle;
 import br.sceweb.modelo.Empresa;
 import br.sceweb.modelo.EmpresaDAO;
 
 public class UC01CadastrarEmpresa {
 	static EmpresaDAO empresaDAO;
 	static Empresa empresa;
-	static Empresa empresa02;
+	static Controle controle;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		empresaDAO = new EmpresaDAO();
+		controle = new Controle();
+		
 		empresa = new Empresa();
 		empresa.setNomeDaEmpresa("empresa x");
 		empresa.setCnpj("89424232000180");
@@ -49,12 +52,29 @@ public class UC01CadastrarEmpresa {
 		
 		try {
 			empresa2.setCnpj("894242320001800");
+
 			fail("deveria disparar uma exception");	
 		} catch (Exception e) {
 			assertEquals("CNPJ inválido!", e.getMessage());
 		}
-		
-		
 	}
 
+	@Test
+	public void CT04UC01Cadastra_Empresa_Atraves_Controle(){
+		controle.excluirEmpresa("73688761000151");
+		controle.cadastrarEmpresa("73688761000151", "Empresa Y", "Y", "Rua xyz", "2323-4455");
+		controle.excluirEmpresa("73688761000151");	
+	}
+	
+	@Test
+	public void CT05UC01Cadastra_Empresa_Nome_Em_Branco(){
+		Empresa empresa02 = new Empresa();
+		
+		try {
+			empresa02.setNomeDaEmpresa("");
+		} catch (Exception e) {
+
+		}
+		
+	}
 }
